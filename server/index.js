d6c16e455dce68421070d92badecb7ba3c4d271d
TheZukab <t1ihal00@students.oamk.fi>
2022-10-17 11:22:02 +0300

    tasks done

--- server/index.js
+++ server/index.js
@@ -1,24 +1,59 @@
 const express = require('express')
 const cors = require('cors')
-const mysql = require('mysql2/promise')
-const config = require('./config')
-
+/* const todoRouter = require('./services/todo') */
+const todoRouter = require('./routes/todo')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+/* app.use('/', router) */
+app.use('/', todoRouter)
 
 const port = 3001
 
+app.use((err, req, res, next) => {
+    const statusCode = err.StatusCode || 500
+    console.error(err.message,err.stack)
+    res.status(statusCode).json({error: err.message})
+    return
+})
+
 app.get("/",async function (req,res) {
     try {
-        const connection = await mysql.createConnection(config.db)
-        const [result,] = await connection.execute('select * from task')
+        /* const connection = await mysql.createConnection(config.db)
+        const [result,] = await connection.execute('select * from task') */
+        /* const result = await db.query('select  * from task') */
+        const result = await todo.getAllTasks()
         if(!result) result=[] //If there is no data, return empty array
         res.status(200).json(result)
     } catch(err){
         // Return status code 500 and error message to the client
+        res.status(500).json({error: err.message})
+    }
+})
+
+
+app.post("/new",async function (req,res) {
+    try {
+        /* const connection = await mysql.createConnection(config.db) */
+        /* const [result,] = await connection.execute('insert into task (description) values (?)', [req.body.description]) */
+        const result = await todo.addTask(req.body)
+        res.status(200).json({id:result.id})
+    } catch(err){
+        // Return status code 500 and error message to the client
+        res.status(500).json({error: err.message})
+    }
+})
+
+app.delete("/delete/:id",async function(req,res){
+    try {
+        /* const connection = await mysql.createConnection(config.db) */
+        // Excute prepared statement
+        /* await connection.execute('delete from task where id = ? ', [req.params.id]) */
+        await todo.removeTask(req.params.id)
+        res.status(200).json({id:req.params.id})
+    } catch(err) {
         res.status(500).json({error: err.message})
     }
 })
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -7,6 +6,7 @@ const URL = 'http://localhost:3001/'
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState('')
 
   useEffect(() => {
     axios.get(URL)
@@ -17,16 +17,51 @@ function App() {
     })
 }, [])
 
-  return (
-    <div>
-    <h3>My tasks</h3>
-    <ol>
-      {tasks.map(task => (
-        <li key={task.id}>{task.description}</li>
-      ))}
-    </ol>
-    </div>
-  );
+return (
+  <div style ={{margin: '20px'}}>
+  <h3>My tasks</h3>
+  <form>
+    <label>Add new</label>
+    <input value={task} onChange={e => setTask(e.target.value)}/>
+    <button type="button" onClick={save}>Save</button>
+  </form>
+  <ol>
+    {tasks.map(task => (
+      <li key={task.id}>{task.description} <a href="new" onClick={() => remove(task.id)}>Delete</a></li>
+    ))}
+  </ol>
+  </div>
+);
+
+function save() {
+  const json = JSON.stringify({description: task})
+  axios.post(URL + 'new',json,{
+    headers: { 'Content-Type': 'application/json'
+  }
+})
+.then((response) => {
+  // Convert stringified JSON object back to Javascript object
+  const addedObject = JSON.parse(json)
+  // Add id returned by the server to object
+  addedObject.id = response.data.id
+  // Update state varibales with newly added data
+  setTasks(tasks => [...tasks, addedObject])
+  setTask('')
+}).catch(error => {
+  alert(error.response.data.error)
+})
+}
+
+function remove(id) {
+  axios.delete(`${URL}delete/${id}`)
+  .then(()=> {
+    const newListWithoutRemoved = tasks.filter((item) => item.id !==id)
+    setTasks(newListWithoutRemoved) 
+  }).catch (error => {
+    alert(error.response.data.error)
+  })
+}
+
 }
 
 export default App;
